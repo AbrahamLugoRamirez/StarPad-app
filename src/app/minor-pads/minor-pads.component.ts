2a68aed52d340257a8ccf337448305ffc731acba
admin <admin@MBP-Abraham.bbrouter>
2024-11-07 22:19:49 -0500

    create minors-pads component

---
+++ src/app/minor-pads/minor-pads.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-minor-pads',
+  templateUrl: './minor-pads.component.html',
+  styleUrls: ['./minor-pads.component.scss'],
+})
+export class MinorPadsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
